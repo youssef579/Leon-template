a27b3356be73de8c3c122a8c85058ed66fd14378
Youssef Ragaey <youssefragaa3@gmail.com>
2022-08-17 20:41:54 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let list = document.querySelector('.list');
+let navList = document.querySelector('header ul');
+let span = document.querySelector(".list div:nth-of-type(even)");
+
+navList.style.display = 'none';
+navList.style.right = document.body.getBoundingClientRect().right - list.getBoundingClientRect().right + 3 + 'px';
+
+list.addEventListener('click', function () {
+    if (navList.style.display == 'none') {
+        navList.style.display = 'block';
+        span.style.width = '100%';
+    } else {
+        span.style.width = '60%';
+        navList.style.display = 'none';
+    }
+})
+
+window.addEventListener("resize", function () {
+    navList.style.right = document.body.getBoundingClientRect().right - list.getBoundingClientRect().right + 3 + 'px';
+});
