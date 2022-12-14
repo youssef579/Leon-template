15adedef68159b06e7f7dc558af70bb3c4575497
Youssef Ragaey <youssefragaa3@gmail.com>
2022-12-14 10:31:57 +0200

    Add fade animation to the floating nav menu

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-let list = document.querySelector('.list');
-let navList = document.querySelector('header ul');
+let list = document.querySelector(".list");
+let navList = document.querySelector("header ul");
 let span = document.querySelector(".list div:nth-of-type(even)");
 
-navList.style.display = 'none';
+navList.style.pointerEvents = "none";
+navList.style.opacity = "0";
 
-function checkResize () {
-    navList.style.right = document.body.getBoundingClientRect().right - list.getBoundingClientRect().right + 3 + 'px';
+function checkResize() {
+    navList.style.right =
+        document.body.getBoundingClientRect().right -
+        list.getBoundingClientRect().right + 3 + "px";
 }
 
-checkResize()
+checkResize();
 
-list.addEventListener('click', function () {
-    if (navList.style.display == 'none') {
-        navList.style.display = 'block';
-        span.style.width = '100%';
+list.addEventListener("click", function () {
+    if (navList.style.opacity == "0") {
+        navList.style.pointerEvents = "auto";
+        navList.style.opacity = "1";
+        navList.style.translate = "0";
+        span.style.width = "100%";
     } else {
-        span.style.width = '60%';
-        navList.style.display = 'none';
+        span.style.width = "60%";
+        navList.style.pointerEvents = "none";
+        navList.style.translate = "0 20px";
+        navList.style.opacity = "0";
     }
-})
+});
 
 window.addEventListener("resize", checkResize);
